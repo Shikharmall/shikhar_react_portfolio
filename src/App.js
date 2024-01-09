78f951be803f1fa0f3f3ea9e306f7a17fe21cf0d
Shikhar Mall <shmall.21.2020@gmail.com>
2024-01-10 02:21:25 +0530

    addded comment points

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Contact from "./components/Contact";
 import Skills from "./components/Skills";
 import Project from "./components/Project";
 import NoPage from "./components/NoPage";
-import { Routes , Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
 function App() {
@@ -17,10 +17,35 @@ function App() {
     window.scroll({ top: 0 });
     document.querySelector("html").style.scrollBehavior = "";
   }, [location.pathname]); // triggered on route change
+
+  //Scope -- boundaries in which we can acccess the variable.
+  //lexical environment -- hiearchy   (lexical use use for nested thing like function ke andar function)  function abc(){ function xyz(){ } }
+  //Lexical scope -- ability given by javascript by which the inner function (function xyz(){}) can access variable his and its parents lexical enivronment variables in its scope(area).
+
+  /*
+  function abc() {
+    var a = 5;
+    function xyz() {
+        console.log(a);
+    }
+    
+    xyz();
+  }
+  
+  abc();
+
+  first find in its local scope then in its parent lexical environment then in global scope.
+  
+  */
+
+  //navigate("/",{state:{user_id:is}});  location.state--->for accesing data in navigated page.
+
   return (
     <>
       <Header />
-      <Routes> {/* wrap all route in routes(plural) */}
+      <Routes>
+        {" "}
+        {/* wrap all route in routes(plural) */}
         <Route path="/" element={<Content />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
